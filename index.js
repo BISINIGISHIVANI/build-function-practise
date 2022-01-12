5ecfc08b605f6d38726e5d4d92229ad6e8796b3b
BISINIGISHIVANI <90177603+BISINIGISHIVANI@users.noreply.github.com>
2022-01-12 22:18:53 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+/*Function
+Easy
+1.Given a and b, your function should return the value of ab
+Example:
+Input: power(2,3) ––> Output: 8*/
+
+const power=(a,b) =>math.pow(a,b);
+power(2,3)
+
+/*
+2.Given length of a regular hexagon, your function should return area of the hexagon.
+Example:
+Input: areaOfHexagon(10) ––> Output: 259.80
+*/
